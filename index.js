ea75d4930179ee60b05a891fc08efc527166d700
Agbonyin Clement <agbonyinclement@gmail.com>
2026-04-22 01:26:23 -0500

    Admin Board

---
+++ index.js
@@ -0,0 +1,128 @@
+const express = require("express");
+const app = express();
+app.set('view engine', 'ejs')
+const env= require('dotenv')
+require("dotenv").config();
+app.use(express.urlencoded({ extended: true }));
+
+
+let name = "Clement"
+let students = [
+  {
+    name: "Clement",
+    degree: "Crop and Environmental Protection",
+    level: "400Level",
+    cgpa: 4.80
+  },
+  {
+    name: "Akorede",
+    degree: "Computer Science",
+    level: "400L",
+    cgpa: 3.50
+  },
+  {
+    name: "Adebayo",
+    degree: "Agricultural Economics",
+    level: "300L",
+    cgpa: 3.90
+  },
+  {
+    name: "Mary",
+    degree: "Biotechnology",
+    level: "200L",
+    cgpa: 4.20
+  },
+  {
+    name: "John",
+    degree: "Soil Science",
+    level: "500L",
+    cgpa: 3.70
+  }
+];
+
+let age= 10
+
+console.log(students);
+
+
+
+app.get('/about', (req,res)=>{
+
+
+    res.render('index', {students, name, age})
+})
+
+
+
+
+app.get('/hom', (req, res)=>{
+    res.send(students)
+})
+
+
+
+app.get('/login', (req,res)=>{
+
+    res.redirect('/home')
+})
+
+
+app.get('/home', (req,res)=>{
+
+    res.sendFile(__dirname+'/index.html')
+})
+
+app.get('/contact', (req,res)=>{
+
+res.render('allStudents', {students})
+
+})
+
+
+app.post('/delUser/:id', (req, res)=>{
+
+const {id}= req.params
+students.splice(id,1)
+res.render('allStudents', {students})
+
+}) 
+
+app.get('/addUser', (req, res)=>{
+
+    res.render('addUser')
+})
+
+app.post('/addUser', (req, res)=>{
+    console.log(req);
+    const {name, degree, cgpa, level} = req.body
+    students.push(req.body)
+    res.render('allStudents', {students})
+})
+
+
+// Edit student details start//
+
+app.get('/editUsers/:id', (req, res) => {
+    const {id} = req.params
+    const student = students[id];
+    res.render('editUsers', { student, id });
+});
+
+app.post('/editUsers/:id', (req, res) => {
+
+    const {name, degree, cgpa, level} = req.body;
+    const{id} = req.params
+    students.splice(id, 1, req.body)
+    res.render('allStudents', {students});
+});
+
+// Edit student details end//
+
+const myPort=process.env.PORT
+app.listen(myPort, (err) => {
+  if (err) {
+    console.log("I can not start server, pls check error");
+  } else {
+    console.log(`Server started successfully at port ${myPort}`);
+  }
+});
